725c42e5c421fb06175fba4ac917c49c97274152
vishalrindhe <rindhevishal32@gmail.com>
2022-01-17 11:51:55 +0530

    k

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static(__dirname + '/dist/demojasmin'));
 // });
 
 app.get('/*',(req, res)=> {
-    res.sendFile('index.html', {root: 'dist/demojasmin/'})
+    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'})
   });
 app.listen(process.env.PORT || 8080);
